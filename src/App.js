6164de47ad79b9a65591b07635d5fdfd9277269e
mouhib saleh <mouhib.saleh20@gmail.com>
2023-02-21 18:43:31 +0100

    class componeant for products and product

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Products from './Components/Products.js';
+import { Container, Row} from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+    <h1 className="text-center my-4">Our Products</h1>
+    <Row>
+    <Products/>
+    </Row>
+  </Container>
+  
   );
 }
 
